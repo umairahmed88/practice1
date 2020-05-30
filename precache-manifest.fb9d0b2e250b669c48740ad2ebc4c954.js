fb0c0a43965f0e704c4d3e9076303c8a7e9984de
umairahmed88 <umairahmed_88@ymail.com>
2020-05-30 21:16:02 +0500

    Updates

---
+++ precache-manifest.fb9d0b2e250b669c48740ad2ebc4c954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107d1cc60c4f626f45c06c4b7c7020b9",
+    "url": "/practice1/index.html"
+  },
+  {
+    "revision": "bd578a42b7d43455f401",
+    "url": "/practice1/static/css/main.34dba83d.chunk.css"
+  },
+  {
+    "revision": "79eac6f380eea0758f8e",
+    "url": "/practice1/static/js/2.2bea4c4e.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/practice1/static/js/2.2bea4c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd578a42b7d43455f401",
+    "url": "/practice1/static/js/main.2a2bf1c1.chunk.js"
+  },
+  {
+    "revision": "9d8435edb2e18fd0756c",
+    "url": "/practice1/static/js/runtime-main.45bbba97.js"
+  }
+]);
